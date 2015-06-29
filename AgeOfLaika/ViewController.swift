493a0c427028ac87072cf6146fbc7f78816a0884
Omer921 <Omer921@gmail.com>
2015-06-29 21:49:49 +0700

    Real dog years added

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var dogYearsTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let ageMultiplier = 7;
+        var intFromTextField = dogYearsTextField.text.toInt()!;
+        var ageOfDog = intFromTextField * ageMultiplier;
+        dogYearsLabel.text = "Dog Years: " + "\(ageOfDog)";
+        dogYearsTextField.text = "";
+        dogYearsTextField.resignFirstResponder();
+    }
 
+    @IBAction func convertRealButtonPressed(sender: UIButton) {
+        var intFromTextField = dogYearsTextField.text.toInt()!;
+        if (intFromTextField <= 2){
+            dogYearsLabel.text = "Dog Years: " + "\(10.5*Double(intFromTextField))";
+        }
+        else {
+            dogYearsLabel.text = "Dog Years: " + "\((10.5*2) + Double(4*(intFromTextField - 2)))";
+        }
+        dogYearsTextField.text = "";
+        dogYearsTextField.resignFirstResponder();
+    }
 }
 
